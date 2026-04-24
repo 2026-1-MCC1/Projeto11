cc978a8ef516c3710a07fec49a06070df4e2932b
Lucas Elinou Feitosa <luskafeitosa0@gmail.com>
2026-04-24 18:22:50 -0300

    arrumando uns bugs
    fiz uma baita besteira e vou ter que refazer

--- documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/clickspawner.cs
+++ documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/clickspawner.cs
@@ -2,24 +2,8 @@ using UnityEngine;
 
 public class ClickSpawner : MonoBehaviour
 {
-    public GameObject prefabTextoFlutuante;
-    public GameObject prefabParticula;
-    // Essa função roda sozinha quando você clica em algo que tem Collider
     void OnMouseDown()
     {
-        Vector3 posicaoDeNascimento = transform.position + Vector3.up * 0f;
-
-        if (prefabTextoFlutuante != null)
-        {
-            GameObject novoTexto = Instantiate(prefabTextoFlutuante, posicaoDeNascimento, Quaternion.identity);
-            novoTexto.transform.LookAt(Camera.main.transform);
-            novoTexto.transform.Rotate(0, 180, 0);
-        }
-
-        if (prefabParticula != null)
-        {
-            Instantiate(prefabParticula, posicaoDeNascimento, Quaternion.identity);
-        }
+        Debug.Log("CLICOU!");
     }
 }
-
