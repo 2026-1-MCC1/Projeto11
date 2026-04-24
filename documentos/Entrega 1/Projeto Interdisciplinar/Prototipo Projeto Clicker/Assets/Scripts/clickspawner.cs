6a404c458c8bdc5b024f467d295a2d3800319f22
Lucas Elinou Feitosa <luskafeitosa0@gmail.com>
2026-04-24 12:09:17 -0300

    Adicionei versão inicial #15

--- documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/clickspawner.cs
+++ documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/clickspawner.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 public class ClickSpawner : MonoBehaviour
 {
-    [Header("Arraste o Cubo Azul do Texto aqui")]
     public GameObject prefabTextoFlutuante;
-
+    public GameObject prefabParticula;
     // Essa função roda sozinha quando você clica em algo que tem Collider
     void OnMouseDown()
     {
-        // 1. Criar o texto um pouco acima do computador
-        // O Vector3(0, 2, 0) joga ele 2 metros para cima
-        Vector3 posicaoDeNascimento = transform.position + Vector3.up * 2f;
+        Vector3 posicaoDeNascimento = transform.position + Vector3.up * 0f;
 
-        // 2. Faz o "carimbo" (Instantiate)
-        GameObject novoTexto = Instantiate(prefabTextoFlutuante, posicaoDeNascimento, Quaternion.identity);
+        if (prefabTextoFlutuante != null)
+        {
+            GameObject novoTexto = Instantiate(prefabTextoFlutuante, posicaoDeNascimento, Quaternion.identity);
+            novoTexto.transform.LookAt(Camera.main.transform);
+            novoTexto.transform.Rotate(0, 180, 0);
+        }
 
-        // 3. (Opcional) Se o texto estiver de costas, essa linha faz ele olhar para a câmera
-        novoTexto.transform.LookAt(Camera.main.transform);
-        novoTexto.transform.Rotate(0, 180, 0);
-
-        Debug.Log("Cliquei no computador e criei o texto!");
+        if (prefabParticula != null)
+        {
+            Instantiate(prefabParticula, posicaoDeNascimento, Quaternion.identity);
+        }
     }
 }
+
--- documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/texto flutuante.cs
+++ documentos/Entrega 1/Projeto Interdisciplinar/Prototipo Projeto Clicker/Assets/Scripts/texto flutuante.cs
@@ -4,14 +4,12 @@ public class TextoFlutuante : MonoBehaviour
 {
     void Start()
     {
-        // O número 1.5f é o tempo (1 segundo e meio). 
-        // Depois desse tempo, o texto se destrói!
-        Destroy(gameObject, 1.5f);
     }
 
     void Update()
     {
         // Isso faz o texto ir para cima devagarzinho todos os frames
-        transform.position += Vector3.up * 2f * Time.deltaTime;
+        transform.position += Vector3.up * 0f * Time.deltaTime;
+        Destroy(gameObject, 1.5f);
     }
 }
